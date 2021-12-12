bfb46e3b65c3a33eb0568534850c5ed0bdbfbbbb
Narek Boshyan <narek.boshyan@gmail.com>
2021-12-12 21:02:13 +0400

    Adding Steps Recursion

--- 09--Printing-Steps/index.js
+++ 09--Printing-Steps/index.js
@@ -32,18 +32,20 @@
 //   }
 // }
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-    for (let col = 0; col < n; col++) {
-      if (col <= row) {
-        stair += "#";
-      } else {
-        stair += " ";
-      }
-    }
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
     console.log(stair);
+    steps(n, row + 1);
+  }
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
   }
 }
 
 module.exports = steps;
+ds;
